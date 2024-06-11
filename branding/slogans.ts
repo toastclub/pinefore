870b8445808b39a78c167ef8441a5a460fd35c30
Evan Boehs <evan@boehs.org>
2024-06-10 22:51:38 -0400

    slongs

---
+++ branding/slogans.ts
@@ -0,0 +1,5 @@
+export const slogans = [
+  "The web as it was",
+  "Reconnect with the web at large",
+  "Meticulously designed tools for collecting and discovering indiependent content around the web.",
+];
