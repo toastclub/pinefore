06a48d928dd52251b967b33ec7f0a099a56bb9c3
Evan Boehs <evan@boehs.org>
2024-06-24 18:27:25 -0400

    small

---
+++ docs/api/api.ts
@@ -0,0 +1,9 @@
+export const apiDocObject = {
+  info: {
+    description: `Here may you rest your weary soul, for you have found the API Documentation for the Pinefore suite.
+
+**Important**: This API is highly volatile, especially first party APIs
+
+Please refer to the [overview of the API](/docs/api) for more information, including authentication.`,
+  },
+};
---
+++ docs/api/pin-get.ts
@@ -0,0 +1,18 @@
+import { t } from "elysia";
+
+export const pinGetSchema = {
+  query: t.Object({
+    where: t.Optional(
+      t.String({
+        description:
+          "A powerful filtering engine. Please refer to the [query engine](/docs/query-engine) documentation for more information.",
+      })
+    ),
+    user: t.Optional(t.Numeric({ description: "The user ID to look for" })),
+    scope: t.Optional(
+      t.Union([t.Literal("user"), t.Literal("community"), t.Literal("network")])
+    ),
+    count: t.Optional(t.Numeric({ maximum: 1000, default: 30 })),
+    offset: t.Optional(t.Numeric({ default: 0 })),
+  }),
+};
