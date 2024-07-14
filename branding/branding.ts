4696d3f6f730f8ecc41eed9ff67923259f2c9f27
Evan Boehs <evan@boehs.org>
2024-07-14 17:13:02 -0400

    branding

---
+++ branding/branding.ts
@@ -0,0 +1,22 @@
+export const colors: Record<
+  string,
+  {
+    pri: string;
+  }
+> = {
+  pinefore: {
+    pri: "2d7048",
+  },
+  feedfore: {
+    pri: "8A3E40",
+  },
+  highfore: {
+    pri: "A37D32",
+  },
+};
+
+export const routes = {
+  "/pins": "pinefore",
+  "/feeds": "feedfore",
+  "/notes": "highfore",
+};
--- server/importers/unHTMLEntity.ts
+++
@@ -1,9 +0,0 @@
-export function unHTMLEntity(str: string) {
-  return str
-    .replaceAll("&lt;", "<")
-    .replaceAll("&gt;", ">")
-    .replaceAll("&amp;", "&")
-    .replaceAll("&quot;", '"')
-    .replaceAll("&#39;", "'")
-    .replaceAll("&nbsp;", " ");
-}
