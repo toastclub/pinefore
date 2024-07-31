21ab79707bd6a72cc9c1b9afc01c15f2963ab9e6
Evan Boehs <evan@boehs.org>
2024-07-31 16:16:14 -0400

    add a couple constants

--- constants.ts
+++ constants.ts
@@ -1,1 +1,5 @@
 export const MODE = import.meta.env.MODE;
+export const ENDPOINT =
+  MODE == "development" ? "localhost:3000" : "pinefore.com";
+export const PROTOCOL =
+  import.meta.env.MODE == "development" ? "http://" : "https://";
