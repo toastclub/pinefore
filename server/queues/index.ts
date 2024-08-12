1273e82e5f3a586e31d3266981fa1aa340f0faf8
Evan Boehs <evan@boehs.org>
2024-08-12 10:16:03 -0400

    checkin

--- server/queues/index.ts
+++ server/queues/index.ts
@@ -1,4 +1,8 @@
-export default async function handleQueue(event: Queue, env: Env) {
+import { MessageBatch } from "@cloudflare/workers-types";
+import { Env } from "../../../server/index";
+import { db } from "../../../server/db";
+
+export default async function handleQueue(event: MessageBatch, env: Env) {
   const kysely = db(env.HYPERDRIVE.connectionString);
   const queue = env.MINI_QUEUE;
   if (event.queue == "rss") {
