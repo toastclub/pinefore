52c59485b43dd7ca5ab57a329788776296118031
Evan Boehs <evan@boehs.org>
2024-08-23 15:55:36 -0400

    add connection string

--- server/cron/index.ts
+++ server/cron/index.ts
@@ -4,7 +4,7 @@ import { db } from "$db";
 import rssCron from "./rss";
 
 export async function handleCron(event: ScheduledController, env: Env) {
-  const kysely = db(env.HYPERDRIVE.connectionString);
+  const kysely = db(env.CONNECTION_STRING || env.HYPERDRIVE.connectionString);
   // 12 UTC is 7 AM EST and 1 PM CEST (Berlin)
   // We run at the 23rd minute to be good citizens
   if (event.cron == "25 12 * * *") {
