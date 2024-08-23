c088c7be0262966fbe9ea7d3f2fc3ff418dd7928
Evan Boehs <evan@boehs.org>
2024-08-23 17:52:12 -0400

    rename col

--- server/ranking/rec.ts
+++ server/ranking/rec.ts
@@ -5,7 +5,7 @@ export function recEntities(entity: number, db: Kysely<Database>) {
   return db
     .selectFrom("entities")
     .orderBy(
-      sql`title_embeddings <-> (SELECT title_embeddings FROM entities WHERE id = ${entity})`
+      sql`title_doc_embeddings <-> (SELECT title_doc_embeddings FROM entities WHERE id = ${entity})`
     )
     .where("id", "!=", entity)
     .limit(10);
