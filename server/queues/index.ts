cb143a6bf0e64e1b13f903b19be6e9b2ad3c711f
Evan Boehs <evan@boehs.org>
2024-09-03 08:10:22 -0400

    read connection string

--- server/queues/index.ts
+++ server/queues/index.ts
@@ -10,7 +10,7 @@ export default async function handleQueue(
   event: MessageBatch<MiniQueueMessage>,
   env: Env
 ) {
-  const kysely = db(env.HYPERDRIVE.connectionString);
+  const kysely = db(env.CONNECTION_STRING || env.HYPERDRIVE.connectionString);
   for (const message of event.messages) {
     if (message.body.type == "rss") {
       await feedsQueue(kysely, message.body.body);
