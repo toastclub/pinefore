2c1fe5f2a2df51c1a815c51bd2d85018567a7e9f
Evan Boehs <evan@boehs.org>
2024-09-08 18:44:14 -0400

    debugging

--- server/queues/index.ts
+++ server/queues/index.ts
@@ -10,6 +10,10 @@ export default async function handleQueue(
   event: MessageBatch<MiniQueueMessage>,
   env: Env
 ) {
+  if (!env.CONNECTION_STRING && !env.HYPERDRIVE.connectionString) {
+    console.log(env);
+    throw new Error("No connection string provided");
+  }
   const kysely = db(env.CONNECTION_STRING || env.HYPERDRIVE.connectionString);
   for (const message of event.messages) {
     if (message.body.type == "rss") {
